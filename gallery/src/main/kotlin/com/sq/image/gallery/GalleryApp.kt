4987cd4e2ef78f62d8f80108dab0c5b91bda85bd
Sascha Rodekamp <rodekamp@smartsquare.de>
2018-07-01 16:30:28 +0200

    add cloud credentials check

--- gallery/src/main/kotlin/com/sq/image/gallery/GalleryApp.kt
+++ gallery/src/main/kotlin/com/sq/image/gallery/GalleryApp.kt
@@ -2,10 +2,26 @@ package com.sq.image
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @SpringBootApplication
 class GalleryApp
 
 fun main(args: Array<String>) {
+
+    checkCloudCredentials()
+
     SpringApplication.run(GalleryApp::class.java, *args)
+}
+
+private fun checkCloudCredentials() {
+    val message = "GOOGLE_APPLICATION_CREDENTIALS environment variable not set. Can not access GCP account. Check: https://cloud.google.com/docs/authentication/getting-started"
+    val gccEnv = System.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+    when {
+        gccEnv == null -> throw IllegalStateException(message)
+        gccEnv.isEmpty() -> throw IllegalStateException(message)
+        !Files.exists(Paths.get(gccEnv)) -> throw IllegalStateException(message)
+    }
 }
