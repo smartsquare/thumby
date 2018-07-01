e74a0fc9d9ddc4bbc418f8e1df2cee33fffc983a
Sascha Rodekamp <rodekamp@smartsquare.de>
2018-07-01 16:10:00 +0200

    add basic image gallery

--- gallery/src/main/kotlin/com/sq/image/gallery/controller/ImageController.kt
+++ gallery/src/main/kotlin/com/sq/image/gallery/controller/ImageController.kt
@@ -1,6 +1,7 @@
 package com.sq.image.controller
 
 import com.sq.image.service.ImageService
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,6 +11,11 @@ class ImageController(val imageService: ImageService) {
     @GetMapping("/image/list")
     fun list(): List<String> {
         return imageService.listAllImagesFromBucket()
+    }
+
+    @GetMapping("/image", produces = [MediaType.IMAGE_JPEG_VALUE])
+    fun loadImage(name: String): String {
+        return imageService.retrieveImageByFilename(name)
     }
 
 }
--- gallery/src/main/kotlin/com/sq/image/gallery/service/ImageService.kt
+++ gallery/src/main/kotlin/com/sq/image/gallery/service/ImageService.kt
@@ -5,6 +5,7 @@ import com.google.cloud.storage.Storage
 import com.google.cloud.storage.StorageOptions
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.util.Base64Utils
 
 @Service
 class ImageService(@Value("\${gcp.bucket-name}") val bucket: String,
@@ -15,11 +16,12 @@ class ImageService(@Value("\${gcp.bucket-name}") val bucket: String,
     }
 
     fun listAllImagesFromBucket(): List<String> {
-        return storage.list(bucket, Storage.BlobListOption.prefix(subfolder)).iterateAll().map { b -> b.selfLink }
+        return storage.list(bucket, Storage.BlobListOption.prefix(subfolder)).iterateAll().map { b -> b.name }
     }
 
-    fun retrieveImageByFilename(filename: String): ByteArray {
-        return storage[BlobId.of(bucket, filename)].getContent()
+    fun retrieveImageByFilename(filename: String): String {
+        val content = storage[BlobId.of(bucket, filename)].getContent()
+        return Base64Utils.encodeToString(content)
     }
 
 }
