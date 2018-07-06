17e2ff3afa7724634ee6e9a8e82dcda1518d2a56
Sascha Rodekamp <rodekamp@smartsquare.de>
2018-07-06 15:46:19 +0200

    rename deployment scripts

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-cd upload
+gcpRegistryHost=gcr.io
+gcpProject=$(gcloud config get-value project)
 
-docker build -t gcr.io/carbide-parser-205909/image-upload:1 .
+cd upload
+docker build -t $gcpRegistryHost/$gcpProject/upload-service:1 .
 
 cd ../gallery
-docker build -t gcr.io/carbide-parser-205909/image-gallery:1 .
+docker build -t $gcpRegistryHost/$gcpProject/gallery-service:1 .
 
 cd ../thumb-generator
-docker build -t gcr.io/carbide-parser-205909/thumb-generator:1 .
+docker build -t $gcpRegistryHost/$gcpProject/thumb-generator-service:1 .
 
 cd ..
 
