e61fa8c1b87aadd78f6e1ca62b92b7ea29387147
Sascha Rodekamp <rodekamp@smartsquare.de>
2018-07-23 12:53:31 +0200

    update build script

--- build.sh
+++ build.sh
@@ -5,17 +5,10 @@
 gcpRegistryHost=gcr.io
 gcpProject=$(gcloud config get-value project)
 
-echo "Pushing to repository project: "$gcpProject
-
-cd upload
-docker build -t $gcpRegistryHost/$gcpProject/upload-service:1 .
-
-cd ../gallery
-docker build -t $gcpRegistryHost/$gcpProject/gallery-service:1 .
-
-cd ../thumb-generator
-docker build -t $gcpRegistryHost/$gcpProject/thumb-generator-service:1 .
-
+echo "Tagging container with project: "$gcpProject
+cd upload && docker build -t $gcpRegistryHost/$gcpProject/upload-service:1 .
+cd ../gallery && docker build -t $gcpRegistryHost/$gcpProject/gallery-service:1 .
+cd ../generator && docker build -t $gcpRegistryHost/$gcpProject/thumb-generator-service:1 .
 cd ..
 
 
