9aa09749923d12b2f36f673b7797f4fe32755726
Sascha Rodekamp <rodekamp@smartsquare.de>
2018-10-22 14:28:48 +0200

    just updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,15 @@ import java.util.Properties
 
 plugins {
 
-    val kotlinVersion = "1.2.70"
+    val kotlinVersion = "1.2.71"
 
     base
     kotlin("jvm") version kotlinVersion apply false
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion apply false
-    id("org.springframework.boot") version "2.0.5.RELEASE" apply false
+    id("org.springframework.boot") version "2.0.6.RELEASE" apply false
     id("io.spring.dependency-management") version "1.0.6.RELEASE" apply false
 
-    id("com.google.cloud.tools.jib") version "0.9.11" apply false
+    id("com.google.cloud.tools.jib") version "0.9.13" apply false
 
     id("io.gitlab.arturbosch.detekt") version "1.0.0.RC8" apply true
 }
@@ -38,13 +38,13 @@ allprojects {
         set("gcloud-project", props.getProperty("GCLOUD_PROJECT_ID"))
 
         // version management
-        set("coroutines-version", "0.26.1")
-        set("google-cloud-storage-version", "1.45.0")
-        set("kotlin-version", "1.2.70")
+        set("coroutines-version", "0.30.2")
+        set("google-cloud-storage-version", "1.49.0")
+        set("kotlin-version", "1.2.71")
         set("hamcrest-version", "2.0.0.0")
         set("json-path-version", "2.4.0")
-        set("mockk-version", "1.8.7")
-        set("kluent-version", "1.40")
+        set("mockk-version", "1.8.9")
+        set("kluent-version", "1.42")
         set("detekt-version", "1.0.0-gradle-rework-beta3")
 
         set("webjar-vue-version", "2.5.17")
@@ -56,7 +56,7 @@ allprojects {
 
 buildscript {
 
-    val kotlinVersion = "1.2.70"
+    val kotlinVersion = "1.2.71"
 
     repositories {
         jcenter()
@@ -64,7 +64,7 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
-        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.4.RELEASE")
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.6.RELEASE")
     }
 }
 
